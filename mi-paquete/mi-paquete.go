32a612be1180144c4b09293d13e29491cdb27fc6
Tedy Bakali <mbakaliltahiri@hotmail.com>
2025-07-25 19:25:24 -0300

    adding rest of operations

--- mi-paquete/mi-paquete.go
+++ mi-paquete/mi-paquete.go
@@ -1,15 +1,28 @@
 package mi_paquete
 
+import "fmt"
+
+type Calculadora struct {
+	Resultado int
+}
+
 // Ejemplo de función exportada
 func Sumar(a, b int) int {
 	return a + b
 }
 
-// Ejemplo de tipo exportado
-type Calculadora struct {
-	Resultado int
+func (c *Calculadora) Restar(x, y int) {
+	c.Resultado = x - y
 }
 
 func (c *Calculadora) Multiplicar(x, y int) {
 	c.Resultado = x * y
+}
+
+func Dividir(x, y float64) (float64, error) {
+	if y == 0 || y == 0.0 {
+
+		return 0, fmt.Errorf("No se puede dividir por 0")
+	}
+	return x / y, nil
 }
